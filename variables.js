30bf03a7c9a57806015251083514d66a1d342016
dinesh-thurpati <dinesheshan.852@gmail.com>
2025-01-26 18:26:50 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,24 @@
+var name = "piyush goyal"
+var minister = "ministry of heavy industries"
+var age = 67
+var isbjp = true
+
+
+var HomeAddress = {
+    plot : "89 & 90",
+    housenumber : "h.no - 2/3/2",
+    colony : "rajashekhar colony",
+    area : "boduppal",
+    pincode : 500039 ,
+    landmark : "opposite mahakaleshwar temple"
+}
+console.log(HomeAddress)
+
+var  x = 100
+x = "name"
+console.log(x)
+
+let user = "dinesh"
+user = 26
+console.log(typeof user)
+
