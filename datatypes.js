f696a55dcccbbd6d43059a8c5bb15e12b644fd8b
dinesh-thurpati <dinesheshan.852@gmail.com>
2025-01-26 18:21:39 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+let name = "revanth reddy"
+console.log(typeof name)
+
+let age = 52
+console.log(typeof age)
+
+let iscm = true
+console.log(typeof iscm)
+
+let karnataka;
+console.log(typeof karnataka)
+
+let nextcm = null
+console.log(typeof nextcm)
+
+let id = Symbol("id")
+console.log( Symbol("id") == Symbol("id"))
+
+let userinfo = {
+    user: "shan" ,
+    age : 23 ,
+    place : "hyd",
+}
+
+
+let num1 = 23
+let num2 = 24
+let num3 = num1 + num2
+console.log(num3)
+
