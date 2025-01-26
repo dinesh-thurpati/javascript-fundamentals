80f4daeb266c3af3db1b24c701840ffb4e76f8df
dinesh-thurpati <dinesheshan.852@gmail.com>
2025-01-26 18:32:34 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,66 @@
+let a = 234
+let b =  345
+
+console.log(a+b)  // arthimetic operations
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(a%b)
+
+
+
+let  p = 23
+let q = 34
+
+console.log(p > q)    //relational or comparison operators
+console.log(p<q)
+console.log(p==q)
+
+
+let a0 = 434
+let a1 = "434"
+
+console.log(a0==a1)      //relational or comparison operators
+console.log(a0===a1)
+console.log(a0!=a1)
+
+let r = 23
+let s= 45
+let t = 49
+
+// console.log(a>c&&a>b)    //logical operator
+// console.log(b>a&&a>c)       t + t = t
+//                             f + f = f remining false
+
+
+console.log(t>r||s>t)
+console.log(r>t||s>t)    //logical operator
+
+console.log(!true)      //logical operator
+
+
+
+let ab = 23
+let pq = ab           //assignment operator or copying operator
+console.log(pq)
+
+
+
+
+let a2 = 25
+let b2 = ++a        //preincrement operator
+console.log(a2,b2)
+
+let a3 = 22
+let b3 = a3++           //post increment operator
+console.log(a3,b3)
+
+let a4 = 22
+let b4 = --a4         //pre decrfement operator
+console.log(a4,b4)
+
+let a5 = 22
+let b5 = a5--         //post decrement operator
+console.log(a5,b5)
+
+
